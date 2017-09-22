ddd398f3324674047652c18a77567336a9509270
Andrew Rios <andrewrobertrios@gmail.com>
2017-09-21 21:11:05 -0400

    add more to match class

--- Match.swift
+++ Match.swift
@@ -8,6 +8,28 @@
 
 import Foundation
 
-class Match {
+class Match: NSObject, NSCoding {
+    /* need initializer, computer move, player move*, point awarded*/
     
-}
+    override init() {
+    
+    }
+    
+    var matchPoints: Int = 0
+    
+    var computerMove: String = ""
+    
+    var playerMove: String = ""
+    
+    
+    func encode(with aCoder: NSCoder) {
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+    }
+    
+
+} 
+
+
+    
